bd426ed2a5978d8024a0dfdbbfa22e04447aafbb
nanda <nanda@nanda>
2019-09-26 19:20:28 +0700

    commit men

---
+++ src/pbo/ulang/pkg10116458/latihan2/deklarasivariabel/PBOUlang10116458Latihan2DeklarasiVariabel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10116458.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * NAMA     : Rinanda Al Qorrie Akbar
+ * NIM      : 10116458
+ */
+public class PBOUlang10116458Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = "  + nilaiKarakter);
+    }
+    
+}
